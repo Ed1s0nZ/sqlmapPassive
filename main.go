4f4c6e9e8b2bc414b9e444b47eb75e88a61f0056
公明 <83812544+Ed1s0nZ@users.noreply.github.com>
2024-08-20 16:46:43 +0800

    Update main.go

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 const (
 	dir        = "./sqlmapReq"
 	numWorkers = 3   // 调节线程数 （启动3个线程调用sqlmap进行扫描）
-	numTxt     = 100 //一次最多存储多少个要扫描的txt在sqlmapReq文件夹下
+	numTxt     = 100 // 一次最多存储多少个要扫描的txt在sqlmapReq文件夹下
+	proxyPort  = "3231" // 此工具用作被动代理的监听端口
 )
 
 func main() {
